98934a09f0c8c6bcb1b7f9e65c37156aa8ebb2f6
Gabriel Sitorus <sitorusgg@gmail.com>
2022-03-11 01:15:51 +0700

    need to figure out how the explore page grid

--- frontend/src/components/Explore/ExploreImage.js
+++ frontend/src/components/Explore/ExploreImage.js
@@ -1,15 +1,19 @@
 import React from 'react'
+import { useState } from 'react'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { loadPhotos } from '../../store/photos'
+import { Modal } from '../../context/Modal'
 import './Explore.css'
+import SignupForm from '../SignupFromModal/SignupForm'
+import { NavLink } from 'react-router-dom'
 
 const ExploreImage = () => {
     const dispatch = useDispatch()
+    const [showModal, setShowModal] = useState()
     const photos = useSelector(state => state.photos)
     const photo = Object.values(photos)
-    console.log(photo)
-
+    const user = useSelector(state => state.session.user)
     useEffect(() => {
         dispatch(loadPhotos())
     }, [dispatch])
@@ -17,12 +21,36 @@ const ExploreImage = () => {
     return (
         <>
             <div className="explore__page">
-                <h1 className='explore__title'>ExploreImage</h1>
-                <div className="explore__card">
-                    {photo.map(pic => (
-                        <img className='explore_photos' key={pic.id} src={pic.imageUrl} alt='' />
-                    ))}
+                <div className="explore__title">
+                    <p>Easily organize all your photos into beautiful albums to share with friends, family, or even other Flickr members.</p>
+                    {user ?
+                        <NavLink to='/dashboard/albums'>
+                            <button className='explore__signIn'>Go To Your Dashboard!</button>
+                        </NavLink>
+                        : <button className='explore__signIn'
+                            onClick={() => setShowModal(true)}
+                        >Sign Up to Create Memories of Your Own!
+                        </button>
+                    }
+                    {showModal && (
+                        <Modal onClose={() => setShowModal(false)}>
+                            <SignupForm />
+                        </Modal>
+                    )}
                 </div>
+
+                <div className="explore__body">
+                    <div className="explore__info">
+                        {photo.map(pic => (
+                            <div key={pic.id} className="explore__card">
+                                <div className='explore__indivCard'>
+                                    <img className='explore__iamge' key={pic.id} src={pic.imageUrl} alt='' />
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                {/* <h1 className='explore__title'>ExploreImage</h1> */}
             </div>
         </>
     )
