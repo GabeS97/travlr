5b0f648bd8b6baa80bd69d12a8cf217c1f9e1097
Gabriel Sitorus <sitorusgg@gmail.com>
2022-03-13 05:43:59 +0700

    done

--- frontend/src/components/Explore/ExploreImage.js
+++ frontend/src/components/Explore/ExploreImage.js
@@ -10,12 +10,14 @@ import { NavLink } from 'react-router-dom'
 import { getUsers } from '../../store/users'
 
 const ExploreImage = () => {
+    let test = [];
     const dispatch = useDispatch()
     const [showModal, setShowModal] = useState()
     const photos = useSelector(state => state.photos)
     const photo = Object.values(photos)
     const users = useSelector(state => state.users)
     const user = useSelector(state => state.session.user)
+    const choice = photo.find(pic => pic.userId === user?.id)
     const usr = Object.values(users)
 
     useEffect(() => {
@@ -23,12 +25,26 @@ const ExploreImage = () => {
         dispatch(getUsers())
     }, [dispatch])
 
+    for (let i = 0; i < usr.length - 1; i++) {
+        let selectUser = usr[i];
+        // console.log('selectUser iteration', selectUser)
+
+        const choice = photo.find(pic => pic.userId === selectUser?.id)
+        test.push(choice)
+        console.log(test)
+
+    }
+
     return (
         <div className='grid__container'>
             <div className="grid__images">
                 {photo.map(ele => (
                     <>
-                        <img className='image__contents' src={ele.imageUrl} alt='' />
+                        <img className='image__contents' src={ele.imageUrl} alt=''>
+                            {/* {choice && (
+                            <i class="fa-solid fa-user"></i>
+                            )} */}
+                        </img>
                     </>
                 ))}
             </div>
