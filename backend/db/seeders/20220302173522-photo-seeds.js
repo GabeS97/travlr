74fb152ac2bc0ad8e81ce3b81c3ae85573469603
Gabriel Sitorus <sitorusgg@gmail.com>
2022-03-13 14:21:43 +0700

    seeder added

--- backend/db/seeders/20220302173522-photo-seeds.js
+++ backend/db/seeders/20220302173522-photo-seeds.js
@@ -20,6 +20,14 @@ module.exports = {
       {
         userId: 1,
         albumId: null,
+        imageUrl: 'https://images.unsplash.com/photo-1475924156734-496f6cac6ec1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fG5hdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+        content: 'I love this pics',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        userId: 1,
+        albumId: null,
         imageUrl: 'https://images.unsplash.com/photo-1530789253388-582c481c54b0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHRyYXZlbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
         content: 'I love this pics',
         createdAt: new Date(),
