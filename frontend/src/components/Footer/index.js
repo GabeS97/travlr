630933de6eca396a7753f3e349c876d348cade4c
Gabriel Sitorus <sitorusgg@gmail.com>
2022-03-14 11:31:54 +0700

    done

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -5,8 +5,8 @@ const Footer = () => {
     return (
         <div className="containFooter">
             <h5>Developed By Gabriel Sitorus</h5>
-            <a id="linkedin_icon" href="https://www.linkedin.com/in/gabriel-sitorus/"><i className="fab fa-github-square"></i></a>
-            <a id="github_icon" href="https://github.com/GabeS97"><i className="fab fa-linkedin"></i></a>
+            <a id="linkedin_icon" target='_blank' href="https://www.linkedin.com/in/gabriel-sitorus/"><i className="fab fa-linkedin"></i></a>
+            <a id="github_icon" target='_blank' href="https://github.com/GabeS97"><i className="fab fa-github-square"></i></a>
         </div>
 
     )
